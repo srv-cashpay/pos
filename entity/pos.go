e9a457612defa2853a459fab1d51182dc7ff3bb4
Asep rayana <asep.rayana09@gmail.com>
2025-07-06 12:43:11 +0700

    discount add

--- entity/pos.go
+++ entity/pos.go
@@ -16,7 +16,7 @@ type Pos struct {
 	Merchant      entity.MerchantDetail `json:"merchant" gorm:"foreignKey:MerchantID;references:ID"`
 	Account       auth.UserVerified     `json:"account" gorm:"foreignKey:UserID;references:ID"`
 	Product       []byte                `gorm:"type:json" json:"product"`
-	Discount      []byte                `json:"discount" gorm:"foreignKey:MerchantID;references:ID"`
+	Discount      []byte                `gorm:"type:json" json:"discount"`
 	Pay           int                   `gorm:"pay" json:"pay"`
 	Description   string                `gorm:"description" json:"description"`
 	CreatedBy     string                `gorm:"created_by" json:"created_by"`
